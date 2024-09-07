a8bb1661bfbefe29e9c4243aaa14ef917a8bd2c1
Jeffery Abbott <jeff@jefferyabbott.com>
2024-09-07 11:20:30 -0400

    Add cron job to ping sites every hour

--- monitor/check.go
+++ monitor/check.go
@@ -4,7 +4,9 @@ import (
 	"context"
 
 	"encore.app/site"
+	"encore.dev/cron"
 	"encore.dev/storage/sqldb"
+	"golang.org/x/sync/errgroup"
 )
 
 //encore:api public method=POST path=/check/:siteID
@@ -13,6 +15,34 @@ func Check(ctx context.Context, siteID int) error {
 	if err != nil {
 		return err
 	}
+	return check(ctx, site)
+}
+
+//encore:api public method=POST path=/checkall
+func CheckAll(ctx context.Context) error {
+	// get the tracked sites
+	resp, err := site.List(ctx)
+	if err != nil {
+		return err
+	}
+
+	// check up to 8 sites concurrently
+	g, ctx := errgroup.WithContext(ctx)
+	g.SetLimit(8)
+	for _, site := range resp.Sites {
+		site := site
+		g.Go(func() error {
+			return check(ctx, site)
+		})
+	}
+	return g.Wait()
+}
+
+var db = sqldb.NewDatabase("monitor", sqldb.DatabaseConfig{
+	Migrations: "./migrations",
+})
+
+func check(ctx context.Context, site *site.Site) error {
 	result, err := Ping(ctx, site.URL)
 	if err != nil {
 		return err
@@ -24,6 +54,9 @@ func Check(ctx context.Context, siteID int) error {
 	return err
 }
 
-var db = sqldb.NewDatabase("monitor", sqldb.DatabaseConfig{
-	Migrations: "./migrations",
+// cron job to check every hour
+var _ = cron.NewJob("check-all", cron.JobConfig{
+	Title:    "Check all sites",
+	Endpoint: CheckAll,
+	Every:    1 * cron.Hour,
 })
